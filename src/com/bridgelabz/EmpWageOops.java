3e5b8312cf050dd17a11e72eda70525754f8eaaa
ipratikner <pratiknerlikar@gmail.com>
2021-10-23 18:53:58 +0530

    Added: calculated employee wage with condition of total working days or hours is reached

--- src/com/bridgelabz/EmpWageOops.java
+++ src/com/bridgelabz/EmpWageOops.java
@@ -2,10 +2,10 @@ package com.bridgelabz;
 import java.util.Random;
 
     public class EmpWageOops {
-         public static void main(String[] args) {
-             Attendance attendance = new Attendance();
-             //attendance.EmployeeCheck();
-             System.out.println("monthly wage is: "+attendance.EmployeeCheck());
+        public static void main(String[] args) {
+         Attendance attendance = new Attendance();
+         attendance.EmployeeCheck();
+            //System.out.println(attendance.EmployeeCheck());
     }
 }
         class Attendance{
@@ -17,34 +17,40 @@ import java.util.Random;
             static final int NO_OF_WORKING_DAYS=20;
             int dailyWage = 0;
             int monthlyWage = 0;
+            int totalHours =0;
+            int hours = 0;
 
     int GetEmployeePresent(int a) {
         Random r = new Random();
         int empCheck = r.nextInt(a);
         return empCheck;
     }
-    int EmployeeCheck() {
-        int empCheck = GetEmployeePresent(3);
-        switch (empCheck) {
-            case IS_PRESENT: {
-                //System.out.println("Employee is present");
-                dailyWage = WAGE_PER_HOUR * IS_FULL_TIME;
-                break;
+    void EmployeeCheck() {
+        for (int i=0;i<=NO_OF_WORKING_DAYS && totalHours<=100;i++){
+            int empCheck = GetEmployeePresent(3);
+            switch (empCheck) {
+                case IS_PRESENT: {
+                    //System.out.println("Employee is present");
+                    dailyWage = WAGE_PER_HOUR * IS_FULL_TIME;
+                    hours += IS_FULL_TIME;
+                    break;
 
+                }
+                case IS_ABSENT: {
+                    //System.out.println("Employee is Absent");
+                    dailyWage = dailyWage;
+                    break;
+                }
+                default: {
+                    //System.out.println("Employee is Part time");
+                    dailyWage = WAGE_PER_HOUR * IS_PART_TIME;
+                    hours += IS_PART_TIME;
+                    break;
+                }
             }
-            case IS_ABSENT: {
-                //System.out.println("Employee is Absent");
-                dailyWage = dailyWage;
-                break;
-            }
-            default: {
-                //System.out.println("Employee is Part time");
-                dailyWage = WAGE_PER_HOUR * IS_PART_TIME;
-                break;
-            }
-
         }
         monthlyWage = NO_OF_WORKING_DAYS * dailyWage;
-        return monthlyWage;
+        totalHours = hours;
+        System.out.println("Monthly salary : " +monthlyWage + " Total Hours : " +hours);
     }
 }
