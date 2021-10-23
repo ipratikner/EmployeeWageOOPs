25c83af85c9e58ead6d2dd2e1beed403c0387dfb
ipratikner <pratiknerlikar@gmail.com>
2021-10-23 18:28:12 +0530

    Added: calculated employee daily wage

--- src/com/bridgelabz/EmpWageOops.java
+++ src/com/bridgelabz/EmpWageOops.java
@@ -5,12 +5,15 @@ public class EmpWageOops {
 
     public static void main(String[] args) {
 
-        Attendance attendance = new Attendance(); // created object
-        attendance.EmployeeCheck(); // method calling
+        Attendance attendance = new Attendance();
+        attendance.EmployeeCheck();
     }
 }
     class Attendance{
-    final int IS_PRESENT = 1;
+     static final int IS_PRESENT = 1;
+     static final int WAGE_PER_HOUR = 20;
+        static final int IS_FULL_TIME = 8;
+        int dailyWage = 0;
 
     int GetEmployeePresent(int a) {
         Random r = new Random();
@@ -21,7 +24,13 @@ public class EmpWageOops {
         int empCheck = GetEmployeePresent(2);
         if (empCheck == IS_PRESENT) {
             System.out.println("Employee is present");
-        } else
+            dailyWage = WAGE_PER_HOUR * IS_FULL_TIME;
+
+        }
+        else {
             System.out.println("Employee is Absent");
+            dailyWage = dailyWage;
+        }
+        System.out.println("Daily Wage : " + dailyWage);
     }
 }
